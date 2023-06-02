d20c5bcc9acffac7d120b7eadb7376afdf5a096d
yenilikci <m.melihcelik017@outlook.com>
2023-06-02 16:48:14 +0300

    add functionality to generate random colors

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -2,7 +2,8 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+
+	bool isRandom;
 
 	public MainPage()
 	{
@@ -11,12 +12,15 @@ public partial class MainPage : ContentPage
 
 	private void Slider_ValueChanged(object sender, EventArgs e)
 	{
-		var red = sldRed.Value;
-		var green = sldGreen.Value;
-		var blue = sldBlue.Value;
+		if(!isRandom)
+		{
+            var red = sldRed.Value;
+            var green = sldGreen.Value;
+            var blue = sldBlue.Value;
 
-		Color color = Color.FromRgb(red, green, blue);
-		SetColor(color);
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+        }
 	}
 
     private void SetColor(Color color)
@@ -24,6 +28,22 @@ public partial class MainPage : ContentPage
 		btnRandom.BackgroundColor = color;  
 		Container.BackgroundColor = color;
 		lblHex.Text = color.ToHex();
+    }
+
+    private void btnRandom_Clicked(object sender, EventArgs e)
+    {
+		isRandom = true;
+		var random = new Random();
+		var color = Color.FromRgb(
+				random.Next(0, 265),
+				random.Next(0, 265),
+				random.Next(0, 265));
+		SetColor(color);
+
+		sldRed.Value = color.Red;
+		sldGreen.Value = color.Green;
+		sldBlue.Value = color.Blue;
+		isRandom = false;
     }
 }
 
