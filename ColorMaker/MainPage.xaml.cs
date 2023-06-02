ee2d73899f2b39bc8334da400de50cec9330bad2
yenilikci <m.melihcelik017@outlook.com>
2023-06-02 16:40:06 +0300

    add functionality to change colors dynamically

--- ColorMaker/MainPage.xaml.cs
+++ ColorMaker/MainPage.xaml.cs
@@ -8,5 +8,22 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void Slider_ValueChanged(object sender, EventArgs e)
+	{
+		var red = sldRed.Value;
+		var green = sldGreen.Value;
+		var blue = sldBlue.Value;
+
+		Color color = Color.FromRgb(red, green, blue);
+		SetColor(color);
+	}
+
+    private void SetColor(Color color)
+    {
+		btnRandom.BackgroundColor = color;  
+		Container.BackgroundColor = color;
+		lblHex.Text = color.ToHex();
+    }
 }
 
